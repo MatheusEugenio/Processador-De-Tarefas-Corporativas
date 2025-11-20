be8aaa7f49850d56b3b19868c7d737c36bde45d7
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-11-20 00:08:07 -0300

    Remove Id in Tarefa.java

--- ProcessadordeTarefasCorporativas/src/ObjetoDeManipulacao/Tarefa.java
+++ ProcessadordeTarefasCorporativas/src/ObjetoDeManipulacao/Tarefa.java
@@ -4,26 +4,22 @@ import java.util.Objects;
 
 public class Tarefa implements Comparable<Tarefa> {
 
-    private int id;
     private String titulo;
     private String categoria; //
     private String prioridade;
 
     public Tarefa(String titulo, String categoria, String prioridade) {
-        this.id = 0;
         this.titulo = titulo;
         this.categoria = categoria;
         this.prioridade = prioridade;
     }
 
     public Tarefa(String titulo, String categoria) {
-        this.id = 0;
         this.titulo = titulo;
         this.categoria = categoria;
         this.prioridade = "Baixa";
     }
 
-    public int getId(){ return id;}
     public String getTitulo(){ return titulo;}
     public String getCategoria(){ return categoria;}
     public String getPrioridade(){ return prioridade;}
@@ -31,7 +27,7 @@ public class Tarefa implements Comparable<Tarefa> {
 
     @Override
     public String toString() {
-        return "Tarefa de ID=" + id + " | Titulo= " + titulo + " | Categoria= " + categoria +" | Prioridade= " + prioridade;
+        return "Titulo da Tarefa= " + titulo + " | Categoria= " + categoria +" | Prioridade= " + prioridade;
     }
 
     @Override
@@ -43,11 +39,11 @@ public class Tarefa implements Comparable<Tarefa> {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Tarefa tarefa = (Tarefa) o;
-        return id == tarefa.id && Objects.equals(titulo, tarefa.titulo) && Objects.equals(categoria, tarefa.categoria) && Objects.equals(prioridade, tarefa.prioridade);
+        return Objects.equals(titulo, tarefa.titulo) && Objects.equals(categoria, tarefa.categoria) && Objects.equals(prioridade, tarefa.prioridade);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, titulo, categoria, prioridade);
+        return Objects.hash(titulo, categoria, prioridade);
     }
 }
