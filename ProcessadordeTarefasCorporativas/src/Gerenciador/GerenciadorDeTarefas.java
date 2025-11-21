a0f5595fd016b29d1db4eefb0239361d8b9a20f1
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-11-21 16:22:56 -0300

    Adjustments to the methods of the GerenciadorDeTarefas.java, correction of exceptions in other methods.

--- ProcessadordeTarefasCorporativas/src/Gerenciador/GerenciadorDeTarefas.java
+++ ProcessadordeTarefasCorporativas/src/Gerenciador/GerenciadorDeTarefas.java
@@ -1,6 +1,7 @@
 package Gerenciador;
 
 import Exceptions.TarefaInvalidException;
+import FerramentaPersistencia.Persistencia;
 import ObjetoDeManipulacao.Tarefa;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,33 +9,60 @@ import java.util.Set;
 public class GerenciadorDeTarefas {
 
     private Set<Tarefa> listaDeTarefas = new HashSet<>();
+    private Persistencia<Tarefa> persistencia;
 
-    void add(Tarefa tarefa) throws TarefaInvalidException{ //lançar exception personalizada
-
-        try {
-            this.listaDeTarefas.add(tarefa);
-        }catch (Exception e){
-            throw new TarefaInvalidException("A tarefa passada como parâmetro é inválida");
-        }
+    public GerenciadorDeTarefas(){
+        this.persistencia = new Persistencia<>();
     }
 
-    void remove(Tarefa tarefa){ //lançar exception personalizada
+    public void add(Tarefa tarefa) throws NullPointerException{
+        if (tarefa == null){
+            throw new NullPointerException("Tarefa passada como referência é nula!");
+        }
+
+        this.listaDeTarefas.add(tarefa);
+    }
+
+    public void remove(Tarefa tarefa) throws TarefaInvalidException, NullPointerException{
+        if (tarefa == null ) {
+            throw new NullPointerException("Tarefa de referência nula!");
+        }
+
+        if (!listaDeTarefas.contains(tarefa)) {
+            throw new TarefaInvalidException("Tarefa de referência não existe dentro da lista!");
+        }
+
         this.listaDeTarefas.remove(tarefa);
     }
 
-    void remove(String nomeTarefa){
-        this.listaDeTarefas.removeIf(t -> t.getTitulo().equalsIgnoreCase(nomeTarefa));
+    public void remove(String nomeTarefa) throws TarefaInvalidException{
+        if (nomeTarefa.isBlank()){
+            throw new TarefaInvalidException("Referência de Tarefa inválida!");
+        }
+
+        boolean removeu = this.listaDeTarefas.removeIf(t -> t.getTitulo().equalsIgnoreCase(nomeTarefa));
+
+        if (removeu) {
+            System.out.println("Tarefa: \""+nomeTarefa+ "\" removida com sucesso!");
+        }else {
+            throw new TarefaInvalidException("Referência de Tarefa inválida!");
+        }
     }
 
-    void exibirLista(){
+    public void exibirLista(){
+        if (this.listaDeTarefas.isEmpty()) {
+            System.out.println("Tarefa Lista vazia!");
+            return;
+        }
+
         this.listaDeTarefas.forEach(System.out::println);
     }
 
-    void acessarTarefa(Tarefa tarefa){ //lançar exception personalizada
+    public void acessarTarefa(Tarefa tarefa)throws TarefaInvalidException{ //lançar exception personalizada
 
     }
 
-    void acessarTarefa(String nomeTarefa){ //lançar exception personalizada
+    public void acessarTarefa(String nomeTarefa) throws TarefaInvalidException{ //lançar exception personalizada
 
     }
 }
--- ProcessadordeTarefasCorporativas/src/TesteExecutaveis/ExecuteTest.java
+++ ProcessadordeTarefasCorporativas/src/TesteExecutaveis/ExecuteTest.java
@@ -1,7 +1,22 @@
 package TesteExecutaveis;
 
+import Exceptions.TarefaInvalidException;
+import Gerenciador.GerenciadorDeTarefas;
+import ObjetoDeManipulacao.Tarefa;
+
 public class ExecuteTest {
     public static void main(String[] args) {
+        GerenciadorDeTarefas  ger = new GerenciadorDeTarefas();
 
+        try {
+            Tarefa tarefa = null;
+            ger.add(tarefa);
+
+/*      } catch (TarefaInvalidException e) {
+//            System.out.println("ERRO: "+ e.getMessage());
+      */} catch (Exception e) {
+            System.out.print("ERRO GERAL: ");
+            e.printStackTrace();
+        }
     }
 }
